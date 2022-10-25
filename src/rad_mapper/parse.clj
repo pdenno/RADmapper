793d7f71627f11efa4b0c78e450a5fe2c50b5706
Peter Denno <podenno@gmail.com>
2022-10-25 13:00:25 -0400

    Parse.clj for shadow/SHA

--- src/rad_mapper/parse.clj
+++ src/rad_mapper/parse.clj
@@ -1,9 +1,40 @@
 (ns rad-mapper.parse
   "Parse the JSONata-like message mapping language."
   (:require
-   [clojure.pprint :as pp :refer [cl-format]]
+   [clojure.pprint  :refer [cl-format]]
+   [clojure.spec.alpha :as s]
    [rad-mapper.util :as util]
    [taoensso.timbre :as log]))
+
+(def ^:dynamic *debugging?* false)
+
+(defn line-msg
+  "Return a string 'Line <n>: ' or 'Line <n>: <msg', depending on args."
+  ([pstate] (line-msg pstate ""))
+  ([pstate msg & args]
+   (if (not-empty args)
+     (cl-format nil "Line ~A: ~A ~{~A~^, ~}~% ~A" (-> pstate :tokens first :line) msg args pstate)
+     (cl-format nil "Line ~A: ~A~% ~A"            (-> pstate :tokens first :line) msg pstate))))
+
+(def diag (atom nil))
+
+(defn ps-throw
+  "A special throw to eliminate to clean up line-seq and "
+  [pstate msg data]
+  (as-> pstate ?ps
+    (dissoc ?ps :line-seq) ; So REPL won't freak out over the reader being closed...
+    (reset! diag {:pstate ?ps :msg msg :data data})
+    (throw (ex-info (line-msg ?ps msg) data))))
+
+(defn ps-assert
+  "A special s/assert that does ps-throw on an error."
+  [ps]
+  (try (s/assert ::ps ps)
+       (catch #?(:clj Exception :cljs :default) e
+         (ps-throw ps (str "pstate is invalid: " #?(:clj (.getMessage e) :cljs e))
+                   {:tags (:tags ps)
+                    :head (:head ps)
+                    :tokens (:tokens ps)}))))
 
 (defmacro defparse [tag [pstate & keys-form] & body]
   `(defmethod parse ~tag [~'tag ~pstate ~@(or keys-form '(& _))]
